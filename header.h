8d622b9cab586b8429041bd817ddd8cd3ff3e5b3
Basel Sayed <baselmohamed802@gmail.com>
2025-09-14 21:50:06 +0300

    Add function.

--- header.h
+++ header.h
@@ -1,5 +1,6 @@
 #ifndef header
 #define header
 
+int add(int x, int y);
 
 #endif // HEADER_FILE_NAME
--- main.cpp
+++ main.cpp
@@ -4,3 +4,7 @@ int main() {
     // Your code here
     return 0;
 }
+
+int add(int x, int y) {
+    return x + y;
+}
