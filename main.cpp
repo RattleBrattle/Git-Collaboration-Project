a1fc2b82f35e4598f6d006515df3c0b2d907966a
Basel Sayed <baselmohamed802@gmail.com>
2025-09-14 21:54:47 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,21 @@
 #include "header.h"
+#include <iostream>
 
+using namespace std;
 int main() {
-    // Your code here
+    int num1 = 0, num2 = 0, result = 0;
+    char op;
+    cout << "Enter your expression (EX: 3 + 4): ";
+    cin >> num1 >> op >> num2;
+    switch (op) {
+        case '+':
+            result = add(num1, num2);
+            cout << "Result: " << result << endl;
+            break;
+        default:
+            cout << "Unsupported operation." << endl;
+            break;
+    }
     return 0;
 }
 
